af9814a2e1b76f426d734e899c6d70193c1e0ca3
Reema <reemasgera@gmail.com>
2023-07-02 22:09:35 +0530

    will work with Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
@@ -9,7 +9,7 @@ import Skills from './components/Skills';
 import Contact from './components/Contact';
 function App() {
   return (
-    <main>
+    <main className='text-gray-400 bg-gray-900 body font'>
       <Navbar/>
       <About/>
       <Education/>
