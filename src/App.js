b484cebce6a889c15c2457efa2b0a5b985766bca
Reema <reemasgera@gmail.com>
2023-07-02 22:03:19 +0530

    initialized components and imported them

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 function App() {
   return (
     <main>
--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,1 @@
-export default function About() {
-    
-}
+export default function About() {}
